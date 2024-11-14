90300cdd58df2e7ad721b5795b016d097704eb4f
Tavner C Murphy <137843084+tavmurphy1@users.noreply.github.com>
2024-11-14 18:33:44 +0000

    Implemented builtin_unset

--- src/builtins.c
+++ src/builtins.c
@@ -155,7 +155,8 @@ static int
 builtin_unset(struct command *cmd, struct builtin_redir const *redir_list)
 {
   for (size_t i = 1; i < cmd->word_count; ++i) {
-    /* TODO: Unset variables */
+    /* DONE: Unset variables */
+    vars_unset(cmd->words[i]);
   }
   return 0;
 }
